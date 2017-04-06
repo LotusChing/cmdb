77169f515387577eb065cab64112c70456e59fe4
LotusChing <986332598@qq.com>
2017-04-06 15:58:35 +0800

    a little better

---
+++ app/modules/lotus.py
@@ -0,0 +1,7 @@
+# coding: utf8
+def go(**kwargs):
+    return kwargs
+
+
+def no():
+    raise Exception('执行过程中出错')
---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+import logging
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config():
+    SECRET_KEY = os.environ.get("SECRET_KDY") or "abcdefg"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(filename)s- %(levelname)s - %(message)s')
+        file_handler = logging.FileHandler(os.path.join(basedir, 'flask.log'))
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        app.logger.addHandler(file_handler)
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
